0beb571c56163cf2688fbe85a8a7f1514fa5be84
John Otwell <john.otwell@protonmail.com>
2024-05-18 16:44:04 -0400

    jupytext

--- config/plugins/simple.nix
+++ config/plugins/simple.nix
@@ -1,29 +1,20 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   plugins = {
     commentary.enable = true;
-
     conjure.enable = true;
-
+    jupytext.enable = true;
     markdown-preview.enable = true;
-
     marks.enable = true;
-
     neotest.enable = true;
-
-    nix.enable = true;
-
     nix-develop.enable = true;
-
+    nix.enable = true;
     nvim-autopairs.enable = true;
-
     parinfer-rust.enable = true;
-
     # qmk.enable = true;
-
     surround.enable = true;
   };
 
-  extraPlugins = with pkgs.vimPlugins; [vim-sneak];
+  extraPlugins = with pkgs.vimPlugins; [ vim-sneak ];
 
-  extraPackages = with pkgs; [ripgrep fd curl];
+  extraPackages = with pkgs; [ ripgrep fd curl ];
 }
