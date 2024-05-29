52c5ecf14981a36e12628bb5af73123210a1007c
John Otwell <john.otwell@spglobal.com>
2024-05-29 01:23:19 -0400

    dap stuff

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -98,6 +98,7 @@
         "<C-d>" = "cmp.mapping.scroll_docs(4)";
         "<C-Space>" = "cmp.mapping.complete()";
         "<C-e>" = "cmp.mapping.close()";
+        "<C-y>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })";
         "<CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })";
         # "<Tab>" =
         #   # lua
--- config/plugins/dap.nix
+++ config/plugins/dap.nix
@@ -1,6 +1,45 @@
 {pkgs, ...}: {
+  keymaps = [
+    {
+      key = "<leader>du";
+      action = "<cmd>lua require('dapui').toggle()<cr>";
+      options.desc = "Toggle DAP UI";
+    }
+    {
+      key = "<leader>dd";
+      action = "<cmd>DapToggleBreakpoint<cr>";
+      options.desc = "Toggle Breakpoint";
+    }
+    {
+      key = "<leader>dc";
+      action = "<cmd>DapContinue<cr>";
+      options.desc = "Continue";
+    }
+    {
+      key = "<leader>ds";
+      action = "<cmd>DapStepOver<cr>";
+      options.desc = "Step Over";
+    }
+    {
+      key = "<leader>di";
+      action = "<cmd>DapStepInto<cr>";
+      options.desc = "Step Into";
+    }
+    {
+      key = "<leader>do";
+      action = "<cmd>DapStepOut<cr>";
+      options.desc = "Step Out";
+    }
+    {
+      key = "<leader>dt";
+      action = "<cmd>DapTerminate<cr>";
+      options.desc = "Terminate";
+    }
+  ];
+
   plugins.dap = {
     enable = true;
+
     adapters = {
       executables = {
         coreclr = {
@@ -18,13 +57,16 @@
     require("dap").configurations.cs = {
       {
         type = "coreclr",
-        name = "Attach to process",
-        request = "attach",
-        processId = require('dap.utils').pick_process,
+        name = "launch - netcoredbg",
+        request = "launch",
         env = {
-          ASPNETCORE_ENVIRONMENT = 'Development',
+          ASPNETCORE_ENVIRONMENT = "Development",
+          AWS_PROFILE = "bdc-dev",
+          CA_AWS_ENVIRONMENT = "dev"
         },
-        cwd = vim.fn.getcwd(),
+        program = function()
+          return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+        end,
       },
     }
   '';
