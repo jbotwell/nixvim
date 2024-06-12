862417c311ca1176656b26193663f488a7448366
John Otwell <john.otwell@proton.me>
2024-06-12 16:13:50 -0400

    navbuddy stuff

--- config/plugins/navbuddy.nix
+++ config/plugins/navbuddy.nix
@@ -3,4 +3,12 @@
     enable = true;
     lsp.autoAttach = true;
   };
+
+  keymaps = [
+    {
+      key = "<leader>nb";
+      action = "<cmd>Navbuddy<cr>";
+      options.desc = "[N]av[B]uddy";
+    }
+  ];
 }
