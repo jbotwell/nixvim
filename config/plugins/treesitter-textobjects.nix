c0b21554eb6a830a1079fcd58b0e48a52536677e
John Otwell <john.otwell@proton.me>
2024-06-12 20:52:07 -0400

    treesitter text objects

--- config/plugins/treesitter-textobjects.nix
+++ config/plugins/treesitter-textobjects.nix
@@ -1,9 +1,30 @@
 {
   plugins.treesitter-textobjects = {
     enable = true;
-    lspInterop.enable = true;
-    move.enable = true;
-    select.enable = true;
+    move = {
+      enable = true;
+    };
+    select = {
+      enable = true;
+      keymaps = {
+        "af" = {
+          query = "@function.outer";
+          desc = "[A]round [F]unction";
+        };
+        "if" = {
+          query = "@function.inner";
+          desc = "[I]nside [F]unction";
+        };
+        "ac" = {
+          query = "@class.outer";
+          desc = "[A]round [C]lass";
+        };
+        "ic" = {
+          query = "@class.inner";
+          desc = "[I]nside [C]lass";
+        };
+      };
+    };
     swap.enable = true;
   };
 }
