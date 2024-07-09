43075d53b39b08c2831a163c7f607f893839bb64
John Otwell <john.otwell@proton.me>
2024-07-09 12:38:56 -0400

    linting

--- config/plugins/lint.nix
+++ config/plugins/lint.nix
@@ -1,10 +1,14 @@
 {pkgs, ...}: {
   plugins.lint.enable = true;
 
-  plugins.lint.lintersByFt = {
-    json = ["jsonlint"];
-    markdown = ["vale"];
-    sh = ["shellcheck"];
+  plugins.lint = {
+    autoCmd = null;
+    lintersByFt = {
+      json = ["jsonlint"];
+      markdown = ["vale"];
+      nix = ["nix"];
+      sh = ["shellcheck"];
+    };
   };
 
   extraPackages = with pkgs; [shellcheck nodePackages.jsonlint vale];
@@ -14,6 +18,11 @@
       key = "<leader>ln";
       action = "<cmd>lua require('lint').try_lint()<cr>";
       options.desc = "[L]i[N]t";
+    }
+    {
+      key = "<leader>le";
+      action = "<cmd>lua vim.api.nvim_buf_clear_namespace(0, 0, 0, -1)<cr>";
+      options.desc = "[L]int [E]rase";
     }
   ];
 }
